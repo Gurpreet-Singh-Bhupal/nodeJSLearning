8c70a4c5c4f27db94038a405f557b1d3f6522ead
Gurpreet-Singh-Bhupal <68347854+Gurpreet-Singh-Bhupal@users.noreply.github.com>
2024-05-24 21:10:46 +0530

    Add files via upload

---
+++ day2/postMethod.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const app = express();
+
+let PORT = 3000;
+app.use(express.json());
+
+app.listen(PORT,(error)=>{
+    if(!error){
+        console.log('Your server is running fine on port : ',PORT)
+    }
+    else{
+        console.log('Your application is not running ,error : ',error)
+    }
+});
+
+let productsList = [
+    {
+        id:1,
+        name:"Pencil",
+        price:5
+    },
+    {
+        id:2,
+        name:"Amul Butter",
+        price:65
+    },
+    {
+        id:3,
+        name:"Ice cream",
+        price:50
+    },
+    {
+        id:4,
+        name:"Deodrant",
+        price:250
+    },
+    {
+        id:5,
+        name:"Aloo Bhujiya",
+        price:120
+    },
+    {
+        id:6,
+        name:"ToothPaste",
+        price:90
+    },
+    {
+        id:7,
+        name:"Bread",
+        price:40
+    },
+    {
+        id:8,
+        name:"Cold drinks",
+        price:60
+    },
+    {
+        id:9,
+        name:"ToothBrush",
+        price:30
+    },
+    {
+        id:10,
+        name:"Pen",
+        price:10
+    },
+    {
+        id:11,
+        name:"Amul Cheese",
+        price:80
+    },
+    {
+        id:12,
+        name:"Chips",
+        price:20
+    },
+    {
+        id:13,
+        name:"Pencil",
+        price:5
+    },
+    {
+        id:14,
+        name:"Plates",
+        price:170
+    },
+     {
+        id:15,
+        name:"ToothPaste",
+        price:90
+    },
+    {
+        id:16,
+        name:"Face Wash",
+        price:200
+    },
+    {
+        id:17,
+        name:"LunchBox",
+        price:250
+    },
+    {
+        id:18,
+        name:"Lays",
+        price:20
+    },
+    {
+        id:19,
+        name:"Hide n Seek",
+        price:40
+    },
+    {
+        id:20,
+        name:"Maggie",
+        price:50
+    },
+]
+
+app.post('/addProduct',(req,res)=>{
+    let newProduct = {
+        id:productsList.length+1,
+        name:req.body.name,
+        price:req.body.price
+    }
+    productsList.push(newProduct);
+    res.send({
+        data:productsList
+    })
+})
